ee8a60b0fe2e8a0757fb370e00f57d61991be673
Kuni Ivanov <inoxydabl3@gmail.com>
2022-07-08 00:46:16 +0200

    feat: Update scheme and data sql
    2 new tables - user, role +insert new data for these tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,34 @@
-insert into customers (name, surname, photo) values ('Kassie','Tonn','');
-insert into customers (name, surname, photo) values ('Efram','Finn','');
-insert into customers (name, surname, photo) values ('Nereyda','Phinney','');
-insert into customers (name, surname, photo) values ('Carrol','Mckinsey','');
-insert into customers (name, surname, photo) values ('Liba','Conant','');
-insert into customers (name, surname, photo) values ('Marlicia','Eggers','');
-insert into customers (name, surname, photo) values ('Allyson','Scalise','');
-insert into customers (name, surname, photo) values ('Blythe','Sweatman','');
-insert into customers (name, surname, photo) values ('Hilario','Hove','');
-insert into customers (name, surname, photo) values ('Clayton','Just','');
-insert into customers (name, surname, photo) values ('Chery','Canady','');
-insert into customers (name, surname, photo) values ('Raman','Grimsley','');
-insert into customers (name, surname, photo) values ('Norman','Hafer','');
-insert into customers (name, surname, photo) values ('Rebekan','Brigman','');
-insert into customers (name, surname, photo) values ('Fidencio','Wenzel','');
-insert into customers (name, surname, photo) values ('Nicolette','Chasse','');
-insert into customers (name, surname, photo) values ('Kristine','Banner','');
-insert into customers (name, surname, photo) values ('Tauheedah','Stutz','');
-insert into customers (name, surname, photo) values ('Katiria','Bent','');
-insert into customers (name, surname, photo) values ('Shonteria','Mcafee','');
-insert into customers (name, surname, photo) values ('Charlana','Rester','');
-insert into customers (name, surname, photo) values ('Annabelle','Laclair','');
-insert into customers (name, surname, photo) values ('Graig','Reider','');
-insert into customers (name, surname, photo) values ('Phylicia','Lash','');
-insert into customers (name, surname, photo) values ('Katira','Spraggins','');
+insert into roles (id, role) values (0, 'ADMIN'), (1, 'USER');
+
+insert into customers (name, surname, photo) values
+    ('Kassie','Tonn',''),
+    ('Efram','Finn',''),
+    ('Nereyda','Phinney',''),
+    ('Carrol','Mckinsey',''),
+    ('Liba','Conant',''),
+    ('Marlicia','Eggers',''),
+    ('Allyson','Scalise',''),
+    ('Blythe','Sweatman',''),
+    ('Hilario','Hove',''),
+    ('Clayton','Just',''),
+    ('Chery','Canady',''),
+    ('Raman','Grimsley',''),
+    ('Norman','Hafer',''),
+    ('Rebekan','Brigman',''),
+    ('Fidencio','Wenzel',''),
+    ('Nicolette','Chasse',''),
+    ('Kristine','Banner',''),
+    ('Tauheedah','Stutz',''),
+    ('Katiria','Bent',''),
+    ('Shonteria','Mcafee',''),
+    ('Charlana','Rester',''),
+    ('Annabelle','Laclair',''),
+    ('Graig','Reider',''),
+    ('Phylicia','Lash',''),
+    ('Katira','Spraggins','')
+;
+
+insert into users (username, password, id_role) values
+    ('admin', '$2a$10$rKRGKXVF3T0icaJIkDes9.ovFDuLjPfic/KMmvXSK/1MdwxTQyuuW', 0),   -- (admin, password)
+    ('user', '$2a$10$rKRGKXVF3T0icaJIkDes9.ovFDuLjPfic/KMmvXSK/1MdwxTQyuuW', 1)     -- (user, password)
+;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,26 @@
-DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS roles, users, customers;
+
+CREATE TABLE roles (
+    id int not null,
+    role varchar(10) not null,
+    primary key(id)
+);
+
+CREATE TABLE users (
+    id int not null auto_increment,
+    username varchar(50) not null,
+    password varchar(60) not null,
+    id_role int,
+    primary key(id),
+    foreign key(id_role) references roles(id) on delete set null on update cascade
+);
 
 CREATE TABLE customers (
-    id int not null auto_increment primary key,
+    id int not null auto_increment,
     name varchar(255) not null,
     surname varchar(255) not null,
-    photo varchar(255)
+    photo varchar(255),
+    id_user int,
+    primary key(id),
+    foreign key(id_user) references users(id) on delete set null on update cascade
 );
