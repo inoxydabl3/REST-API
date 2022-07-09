71eec9d5bd06321e0c2783cb13322429406a7687
Kuni Ivanov <inoxydabl3@gmail.com>
2022-07-09 04:15:53 +0200

    feat: New security measures - Authorisation, XSS prevention

--- src/main/java/com/example/demo/configurations/SecurityConfig.java
+++ src/main/java/com/example/demo/configurations/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.example.demo.configurations;
 
 import com.example.demo.services.database.DatabaseUserDetailsService;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,14 +13,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Order(SecurityProperties.BASIC_AUTH_ORDER - 10)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests()
+                .antMatchers("/customers/**").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/users/**").hasAnyAuthority("ADMIN")
                 .and()
                 .httpBasic();
+        http
+                .headers()
+                .contentSecurityPolicy("script-src 'self'");
         return http.build();
     }
 
