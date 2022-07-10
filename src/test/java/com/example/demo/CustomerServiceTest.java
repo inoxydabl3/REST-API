7f12e16aa18c2ceb841c971e33bee4b15e737fe6
Kuni Ivanov <inoxydabl3@gmail.com>
2022-07-10 02:17:06 +0200

    test: Some tests

---
+++ src/test/java/com/example/demo/CustomerServiceTest.java
@@ -0,0 +1,215 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.example.demo.dtos.CustomerCreationDTO;
+import com.example.demo.dtos.CustomerDTO;
+import com.example.demo.exceptions.ImageStorageException;
+import com.example.demo.services.customer.CustomerService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Random;
+
+@Slf4j
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@ActiveProfiles("test")
+class CustomerServiceTest {
+
+	private static final Random RND = new Random();
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+	public static final int LOWER_BOUND = 5000;
+	public static final int UPPER_BOUND = 10000;
+
+	private static final String USER = "user";
+	private static final String ADMIN = "admin";
+
+	private static final int UPDATED_CUSTOMER = 1;
+	private static final int DELETED_CUSTOMER = 2;
+
+	private static final String PHOTO_NAME = "photo.jpg";
+	private static final String CONTENT_TYPE = "image/jpeg";
+
+	private static int generateRandomInt() {
+		return RND.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+	}
+
+	private final CustomerService service;
+
+	@AfterAll
+	static void remove(@Value("${imagesPath}") Path image) {
+		FileSystemUtils.deleteRecursively(image.toFile());
+	}
+
+	@Test
+	void contextLoads() {
+		assertNotNull(service);
+		log.info("Context have loaded successfully");
+	}
+
+	@Test
+	void getAll() {
+		List<CustomerDTO> customers = service.getAllCustomersSummary();
+		assertThat(customers).isNotNull().isNotEmpty();
+		log.info("Customers fetched successfully: {}", customers.size());
+		customers.forEach(c -> log.info(String.valueOf(c)));
+	}
+
+	@Test
+	void get() {
+		int customerId = UPDATED_CUSTOMER;
+		Optional<CustomerDTO> customer = service.getCustomer(customerId);
+		assertThat(customer).isPresent();
+		assertThat(customer.get())
+				.matches(c -> customerId == c.getId(), "customerId");
+		log.info("Customer fetched successfully");
+		log.info("{}", GSON.toJson(customer.get()));
+	}
+
+	@Test
+	void getNonExisting() {
+		int customerId = generateRandomInt();
+		Optional<CustomerDTO> customer = service.getCustomer(customerId);
+		assertThat(customer).isEmpty();
+		log.info("Customer with id '{}' is not existing", customerId);
+	}
+
+	@Test
+	void createWithoutPhoto() throws ImageStorageException {
+		String name = "createWithoutPhoto";
+		String surname = "createWithoutPhoto";
+		String userRef = USER;
+		CustomerDTO customer = service.createCustomer(CustomerCreationDTO.builder()
+				.name(name)
+				.surname(surname)
+				.userRef(userRef)
+				.build());
+		assertThat(customer).isNotNull()
+				.matches(c -> name.equals(c.getName()), "name")
+				.matches(c -> surname.equals(c.getSurname()), "surname")
+				.matches(c -> userRef.equals(c.getUserRef()), "userRef")
+				.matches(c -> c.getPhoto() == null, "photo");
+		log.info("Customer created successfully");
+		log.info("{}", GSON.toJson(customer));
+	}
+
+	@Test
+	void createWithPhoto() throws ImageStorageException, IOException {
+		MultipartFile photo = new MockMultipartFile(PHOTO_NAME, PHOTO_NAME, CONTENT_TYPE,
+				Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(PHOTO_NAME)));
+		String name = "createWithPhoto";
+		String surname = "createWithPhoto";
+		String userRef = USER;
+		CustomerDTO customer = service.createCustomer(CustomerCreationDTO.builder()
+				.name(name)
+				.surname(surname)
+				.userRef(userRef)
+				.photo(photo)
+				.build());
+		assertThat(customer).isNotNull()
+				.matches(c -> name.equals(c.getName()), "name")
+				.matches(c -> surname.equals(c.getSurname()), "surname")
+				.matches(c -> userRef.equals(c.getUserRef()), "userRef")
+				.matches(c -> c.getPhoto() != null, "photo");
+		log.info("Customer created successfully");
+		log.info("{}", GSON.toJson(customer));
+	}
+
+	@Test
+	void updatePartial() throws ImageStorageException {
+		int customerId = UPDATED_CUSTOMER;
+		String name = "updatePartial";
+		String userRef = ADMIN;
+		Optional<CustomerDTO> maybeCustomer = service.updateCustomer(customerId, CustomerCreationDTO.builder()
+				.name(name)
+				.userRef(userRef)
+				.build());
+		assertThat(maybeCustomer).isPresent();
+		CustomerDTO customer = maybeCustomer.get();
+		assertThat(customer)
+				.matches(c -> customerId == c.getId(), "customerId")
+				.matches(c -> name.equals(c.getName()), "name")
+				.matches(c -> c.getSurname() != null, "surname")
+				.matches(c -> userRef.equals(c.getUserRef()), "userRef");
+		log.info("Customer updated successfully");
+		log.info("{}", GSON.toJson(customer));
+	}
+
+	@Test
+	void updateFull() throws IOException, ImageStorageException {
+		MultipartFile photo = new MockMultipartFile(PHOTO_NAME, PHOTO_NAME, CONTENT_TYPE,
+				Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(PHOTO_NAME)));
+		int customerId = UPDATED_CUSTOMER;
+		String name = "updateFull";
+		String surname = "updateFull";
+		String userRef = USER;
+		Optional<CustomerDTO> maybeCustomer = service.updateCustomer(customerId, CustomerCreationDTO.builder()
+				.name(name)
+				.surname(surname)
+				.userRef(userRef)
+				.photo(photo)
+				.build());
+		assertThat(maybeCustomer).isPresent();
+		CustomerDTO customer = maybeCustomer.get();
+		assertThat(customer)
+				.matches(c -> customerId == c.getId(), "customerId")
+				.matches(c -> name.equals(c.getName()), "name")
+				.matches(c -> surname.equals(c.getSurname()), "surname")
+				.matches(c -> userRef.equals(c.getUserRef()), "userRef")
+				.matches(c -> c.getPhoto() != null, "photo");
+		log.info("Customer updated successfully");
+		log.info("{}", GSON.toJson(customer));
+	}
+
+	@Test
+	void updateNonExisting() throws ImageStorageException {
+		int customerId = generateRandomInt();
+		Optional<CustomerDTO> customer = service.updateCustomer(customerId, CustomerCreationDTO.builder()
+				.name("updateNonExisting")
+				.userRef(ADMIN)
+				.build());
+		assertThat(customer).isEmpty();
+		log.info("Customer with id '{}' is not existing", customerId);
+	}
+
+	@Test
+	void delete() {
+		int customerId = DELETED_CUSTOMER;
+		Optional<CustomerDTO> customer = service.deleteCustomer(customerId);
+		assertThat(customer).isPresent();
+		assertThat(customer.get().getId()).isEqualTo(customerId);
+		log.info("Customer delete successfully");
+		log.info("{}", GSON.toJson(customer.get()));
+	}
+
+	@Test
+	void deleteNonExisting() {
+		int customerId = generateRandomInt();
+		Optional<CustomerDTO> customer = service.deleteCustomer(customerId);
+		assertThat(customer).isEmpty();
+		log.info("Customer with id '{}' is not existing", customerId);
+	}
+
+}
---
+++ src/test/java/com/example/demo/UserServiceTest.java
@@ -0,0 +1,183 @@
+package com.example.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import com.example.demo.dtos.UserCreationDTO;
+import com.example.demo.dtos.UserDTO;
+import com.example.demo.services.user.UserSerivce;
+import com.example.demo.utils.Role;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Slf4j
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class UserServiceTest {
+
+    private static final Random RND = new Random();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static final int LOWER_BOUND = 5000;
+    public static final int UPPER_BOUND = 10000;
+
+    private static final int UPDATED_USER = 1;
+    private static final int DELETED_USER = 2;
+
+    private static int generateRandomInt() {
+        return RND.nextInt(UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
+    }
+
+    private final UserSerivce service;
+    private final PasswordEncoder passwordEncoder;
+
+    @Test
+    @Order(1)
+    void contextLoads() {
+        assertNotNull(service);
+        assertNotNull(passwordEncoder);
+        log.info("Context have loaded successfully");
+    }
+
+    @Test
+    void get() {
+        int userId = UPDATED_USER;
+        Optional<UserDTO> user = service.getUser(userId);
+        assertThat(user).isPresent();
+        assertThat(user.get())
+                .matches(u -> userId == u.getId(), "userId")
+                .hasNoNullFieldsOrProperties();
+        log.info("User fetched successfully");
+        log.info("{}", GSON.toJson(user.get()));
+    }
+
+    @Test
+    void getNonExisting() {
+        int userId = generateRandomInt();
+        Optional<UserDTO> user = service.getUser(userId);
+        assertThat(user).isEmpty();
+        log.info("User with id '{}' is not existing", userId);
+    }
+
+    @Test
+    void create() {
+        String username = "createUser";
+        String password = "createUser";
+        Role role = Role.USER;
+        UserDTO user = service.createUser(UserCreationDTO.builder()
+                .username(username)
+                .password(password)
+                .role(role)
+                .build());
+        assertThat(user).isNotNull().hasNoNullFieldsOrProperties()
+                .matches(u -> username.equals(u.getUsername()), "username")
+                .matches(u -> passwordEncoder.matches(password, u.getPassword()), "encoded password")
+                .matches(u -> role.equals(u.getRole()), "role");
+        log.info("User created successfully");
+        log.info("{}", GSON.toJson(user));
+    }
+
+    @Test
+    void updatePartial() {
+        int userId = UPDATED_USER;
+        String password = "updatePartial";
+        Role role = Role.ADMIN;
+        Optional<UserDTO> maybeUser = service.updateUser(userId, UserCreationDTO.builder()
+                .password(password)
+                .role(role)
+                .build());
+        assertThat(maybeUser).isPresent();
+        UserDTO user = maybeUser.get();
+        assertThat(user).hasNoNullFieldsOrProperties()
+                .matches(u -> userId == u.getId(), "userId")
+                .matches(u -> passwordEncoder.matches(password, u.getPassword()), "encoded password")
+                .matches(u -> role.equals(u.getRole()), "role");
+        log.info("User updated successfully");
+        log.info("{}", GSON.toJson(user));
+    }
+
+    @Test
+    void updateFull() {
+        int userId = UPDATED_USER;
+        String username = "updateFull";
+        String password = "updateFull";
+        Role role = Role.USER;
+        Optional<UserDTO> maybeUser = service.updateUser(userId, UserCreationDTO.builder()
+                .username(username)
+                .password(password)
+                .role(role)
+                .build());
+        assertThat(maybeUser).isPresent();
+        UserDTO user = maybeUser.get();
+        assertThat(user).isNotNull().hasNoNullFieldsOrProperties()
+                .matches(u -> userId == u.getId(), "userId")
+                .matches(u -> username.equals(u.getUsername()), "username")
+                .matches(u -> passwordEncoder.matches(password, u.getPassword()), "encoded password")
+                .matches(u -> role.equals(u.getRole()), "role");
+        log.info("User updated successfully");
+        log.info("{}", GSON.toJson(user));
+    }
+
+    @Test
+    void updateNonExisting() {
+        int userId = generateRandomInt();
+        Optional<UserDTO> user = service.updateUser(userId, UserCreationDTO.builder()
+                .role(Role.USER)
+                .build());
+        assertThat(user).isEmpty();
+        log.info("User with id '{}' is not existing", userId);
+    }
+
+    @Test
+    void delete() {
+        int userId = DELETED_USER;
+        Optional<UserDTO> user = service.deleteUser(userId);
+        assertThat(user).isPresent();
+        assertThat(user.get().getId()).isEqualTo(userId);
+        log.info("User delete successfully");
+        log.info("{}", GSON.toJson(user.get()));
+    }
+
+    @Test
+    void deleteNonExisting() {
+        int userId = generateRandomInt();
+        Optional<UserDTO> user = service.deleteUser(userId);
+        assertThat(user).isEmpty();
+        log.info("User with id '{}' is not existing", userId);
+    }
+
+    @Test
+    void changeStatus() {
+        int userId = UPDATED_USER;
+        Role role = Role.ADMIN;
+        Optional<UserDTO> maybeUser = service.changeStats(userId, role);
+        assertThat(maybeUser).isPresent();
+        assertThat(maybeUser.get())
+                .matches(u -> userId == u.getId(), "userId")
+                .matches(u -> role.equals(u.getRole()), "role");
+        log.info("User status updated successfully");
+        log.info("{}", GSON.toJson(maybeUser.get()));
+    }
+
+    @Test
+    void changeStatusNonExisting() {
+        int userId = generateRandomInt();
+        Optional<UserDTO> user = service.changeStats(userId, Role.ADMIN);
+        assertThat(user).isEmpty();
+        log.info("User with id '{}' is not existing", userId);
+    }
+
+}
